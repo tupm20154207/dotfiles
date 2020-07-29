0d9d976a4b68d0c062df40c75008ebf8ebb70783
Tu Pham <phamminhtubkhn@gmail.com>
2020-07-29 22:53:06 +0700

    Synced at 2020-07-29 22:53:06

--- ftplugin/markdown.vim
+++
@@ -1,4 +0,0 @@
-setlocal  tabstop=4 shiftwidth=4 expandtab
-setlocal autoindent smartindent
-setlocal nonumber
-
--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 ln -sfn $(pwd)/config_files/vimrc ~/.vimrc
-ln -sfn $(pwd)/config_files/ftplugin ~/.vim/ftplugin
 ln -sfn $(pwd)/config_files/bash_aliases ~/.bash_aliases
-ln -sfn $(pwd)/config_files/coc-settings.json ~/.vim/coc-settings.json
