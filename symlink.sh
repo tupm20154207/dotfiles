3b2e357ad8a3e8ee10a884f32e36bd3ea577b049
Tu Pham <phamminhtubkhn@gmail.com>
2020-10-19 09:18:44 +0700

    Add zsh support

--- symlink.sh
+++ symlink.sh
@@ -3,3 +3,5 @@
 ln -sfn $(pwd)/config_files/vimrc ~/.vimrc
 ln -sfn $(pwd)/config_files/bash_aliases ~/.bash_aliases
 ln -sfn $(pwd)/ultisnips ~/.vim/UltiSnips
+ln -sfn $(pwd)/config_files/zshrc ~/.zshrc
+ln -sfn $(pwd)/config_files/zsh_aliases ~/.zsh_aliases
